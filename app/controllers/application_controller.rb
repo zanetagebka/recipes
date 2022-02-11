3bf2b713d8fa3196b793b6fc91f57fcbe31a5074
zanetagebka <gebka@jchsoft.cz>
2022-02-11 09:24:47 +0100

    clean up codde

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  def param_preps(param)
-    param.downcase.split(',').map(&:strip).map { |val| "%#{val}%" }
-  end
-
-  helper_method :param_preps
 end
